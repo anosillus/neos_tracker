792bf3a4bf75b1f046a180405f08b66e31f27c70
anosillus <anosillus@gmail.com>
2021-03-08 04:08:15 +0900

    poety setup done

--- aa.sh
+++ aa.sh
@@ -1,4 +1,4 @@
-poetry add pandas python-dotenv requests
+poetry add pandas python-dotenv requests poetry-setup
 poetry add --dev black click coverage flake8 flake8-bandit flake8-black flake8-broken-line flake8-bugbear flake8-builtins flake8-import-order flake8-rst-docstrings flake8-spellcheck flake8-sql flake8-strict flake8-string-format isort logzero pandas-vet pep8 pep8-naming pip ptpython pytest pytest-cov pytest-logger pytest-mock pytest-runner python-language-server reorder-python-imports rstcheck setuptools sphinx tox twine
 
 
